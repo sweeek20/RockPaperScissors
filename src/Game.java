ef8f06df0e411e62d4eeaec84221e5ffeb34585e
Paul Pauca <paucavp@wfu.edu>
2021-12-02 23:14:22 -0500

    fixed action listener

--- src/Game.java
+++ src/Game.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class Game extends JFrame implements ActionListener {
     private JButton rock, paper, scissors;
-    private JTextField text1, text2, text3;
+    private JLabel text1, text2, text3;
     int[] score = new int[2];
 
     public void setup() {
@@ -15,17 +15,17 @@ public class Game extends JFrame implements ActionListener {
         setVisible(true);
         JLabel background=new JLabel(new ImageIcon("rockpaperscissors.png"));
         add(background);
-        background.setLayout(new FlowLayout());
+        background.setLayout(new GridLayout(0, 2));
 
-        text1 = new JTextField(13);
+        text1 = new JLabel();
         background.add(text1);
 
 
-        text2 = new JTextField(13);
+        text2 = new JLabel();
         text2.setBounds(500,250,75,5);
         background.add(text2);
 
-        text3 = new JTextField(13);
+        text3 = new JLabel();
         text3.setBounds(500,100,75,5);
         background.add(text3);
 
@@ -45,65 +45,61 @@ public class Game extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent event) {
+
         Object s = event.getSource();
 
-        while (true) {
-            System.out.println("in actionPerformed");
-            int player = 0;
-            int n = 0;
-            String winner;
+        if (score[0] == 2) {
+            System.out.println("Player wins best out of 3. End of game.");
+            return;
 
-            if (s == rock) {
-                player = 0;
-
-            } else if (s == paper) {
-                player = 1;
-
-            } else if (s == scissors) {
-                player = 2;
-            }
-
-            RPSObject computer1 = new RPSObject(3);
-            computer1.outcome();
-            n = computer1.getValue();
-
-            //output what computer choice was (rock, paper or scissors)
-            if (n == 0) {
-                text2.setText("Computer throws Rock");
-            }
-            if (n == 1) {
-                text2.setText("Computer throws Paper");
-            }
-            if (n == 2) {
-                text2.setText("Computer throws Scissors");
-            }
-
-            //insert winner call to method here
-            winner = winner(player, n);
-            text1.setText("Winner of round: " + winner);
-
-            if (winner.equals("Computer")) {
-                score[1]++;
-            }
-            else if (winner.equals("Player")) {
-                score[0]++;
-            }
-            text3.setText("Player Score: " + score[0] + "  Computer score:  " + score[1]);
-
-
-            if (score[0] < 3 && score[1] < 3) {
-                continue;
-            }
-            if (score[0] == 2) {
-                System.out.println("Player wins best out of 3. End of game.");
-                break;
-
-            }
-            if (score[1] == 2) {
-                System.out.println("Computer wins best out of 3. End of game.");
-                break;
-            }
         }
+        if (score[1] == 2) {
+            System.out.println("Computer wins best out of 3. End of game.");
+            return;
+        }
+
+        int player = 0;
+        int n = 0;
+        String winner;
+
+        if (s == rock) {
+            player = 0;
+
+        } else if (s == paper) {
+            player = 1;
+
+        } else if (s == scissors) {
+            player = 2;
+        }
+
+        RPSObject computer1 = new RPSObject(3);
+        computer1.outcome();
+        n = computer1.getValue();
+
+        //output what computer choice was (rock, paper or scissors)
+        if (n == 0) {
+            text2.setText("Computer throws Rock");
+        }
+        if (n == 1) {
+            text2.setText("Computer throws Paper");
+        }
+        if (n == 2) {
+            text2.setText("Computer throws Scissors");
+        }
+
+        //insert winner call to method here
+        winner = winner(player, n);
+        text1.setText("Winner of round: " + winner);
+
+        if (winner.equals("Computer")) {
+            score[1]++;
+        }
+        else if (winner.equals("Player")) {
+            score[0]++;
+        }
+        text3.setText("Player Score: " + score[0] + "  Computer score:  " + score[1]);
+
+
     }
 
     public static String winner(int player, int n){
